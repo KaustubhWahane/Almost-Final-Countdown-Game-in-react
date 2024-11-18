388d1b7ec76d806b59d8d4ce626c97d8e42692fd
Lavda <kaustubhwahane55@gmail.com>
2024-11-18 12:10:20 +0530

    Use ref over use state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,47 @@
+import { useRef, useState } from "react";
+
+
 export default function Player() {
+  // This is used because we don't want to change the state the moment the user types the name
+  const playerName = useRef();
+  const [enteredPlayer ,  setEnteredPlayer] = useState('');
+  // This is the UseState method to change 
+  // const [submitted ,  setSubmitted] = useState(false);
+
+  // function handleChange(event){
+  //   setEnteredPlayer(event.target.value);
+  // }
+
+  // function handleClick(){
+  //   setSubmitted(true);
+  // }
+
+  function handleClick(){
+    setEnteredPlayer(playerName.current.value);
+  }
+
   return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
+    // <section id="player">
+    //   <h2>Welcome {submitted ? enteredPlayer : "unknown entity"}</h2>
+    //   <p>
+    //     <input type="text" onChange={handleChange}/>
+    //     <button onClick={handleClick}>Set Name</button>
+    //   </p>
+    // </section>
+
+    //This is the UseRef method 
+     <section id="player">
+      <h2>Welcome {enteredPlayer ?? "Unknown Entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input
+         type="text" 
+         ref={playerName} 
+         />
+        <button 
+        onClick={handleClick}
+        >Set Name</button>
       </p>
     </section>
+   
   );
 }
